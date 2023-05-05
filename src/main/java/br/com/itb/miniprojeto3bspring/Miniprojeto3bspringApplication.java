43a0f2d7f049198c5103e1d04cb9c91f1dbe0f1f
programadormovel <programadormovel@gmail.com>
2023-05-05 08:27:31 -0300

    aula inicial

---
+++ src/main/java/br/com/itb/miniprojeto3bspring/Miniprojeto3bspringApplication.java
@@ -0,0 +1,13 @@
+package br.com.itb.miniprojeto3bspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Miniprojeto3bspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Miniprojeto3bspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/itb/miniprojeto3bspring/Miniprojeto3bspringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.itb.miniprojeto3bspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Miniprojeto3bspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
